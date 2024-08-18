94df3b309ddb22704f32962026daf2bc9eafdde9
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2024-08-18 10:55:40 -0500

    fixed crud and file functions

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,29 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import * as fs from 'fs';
+
 const router = express.Router();
+const file = "./allTodos.json"
 
 // Mock database
 let todos = [];
+fs.readFile(file, (error, data) => {
+    if (error) {
+        console.log('Could not read file');
+        throw error;
+    }
+    todos = JSON.parse(data) || [];
+});
+
+function writeJSON(todos) {
+    const data = JSON.stringify(todos, null, 2);
+    fs.writeFile(file, data, error => {
+        if (error) {
+            console.log('Could not write file');
+            throw error;
+        } else console.log('File written successfully');
+    })
+}
 
 // Getting the list of todos from the mock database
 router.get('/', (req, res) => {
@@ -21,8 +42,9 @@ router.get('/:id', (req, res) => {
 // Adding a new todo to our mock database
 router.post('/', (req, res) => {
     const todo = req.body;
-    todos.push({ ...todo, id: todos.length + 1 });
+    todos.push({ ...todo, id: uuidv4() });
     res.send(`${todo.task} has been added to the Database`);
+    writeJSON(todos);
 })
 
 // Changing an existing todo in our mock database
@@ -31,6 +53,7 @@ router.put('/:id', (req, res) => {
     const updatedTodo = req.body;
     todos = todos.map(todo => todo.id === id ? updatedTodo : todo);
     res.send(`${id} has been changed in the Database`);
+    writeJSON(todos);
 })
 
 // Deleting a todo from our mock database
@@ -38,6 +61,7 @@ router.delete('/:id', (req, res) => {
     const id = req.params.id;
     todos = todos.filter(todo => todo.id !== id)
     res.send(`${id} deleted successfully`);
+    writeJSON(todos);
 })
 
 export default router
