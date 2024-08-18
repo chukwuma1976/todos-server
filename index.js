0163b3e2910e1eb51fde803230c8c16e204c5abb
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2024-08-18 13:43:20 -0500

    added cors to allow access

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import todoRoutes from './routes/todos.js'
+import todoRoutes from './routes/todos.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 5000
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/todos', todoRoutes);
 
